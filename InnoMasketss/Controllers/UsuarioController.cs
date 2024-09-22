d488ed037d5b999f7279a54d4065bdff2581e1b4
Joseph <155230237+Joseph303P@users.noreply.github.com>
2024-09-22 17:35:23 -0600

    Agregar archivos de proyecto.

---
+++ InnoMasketss/Controllers/UsuarioController.cs
@@ -0,0 +1,80 @@
+﻿using InnoMasketss.Data.Servicios;
+using InnoMasketss.Data;
+using InnoMasketss.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace InnoMasketss.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly Contexto _contexto;
+        private readonly UsuarioServicio _usuarioServicio;
+
+        public UsuarioController(Contexto contexto)
+        {
+            _contexto = contexto;
+            _usuarioServicio = new UsuarioServicio(contexto);
+        }
+
+        //Metodo de perfil de usuario
+        [Authorize]
+        public ActionResult Perfil()
+        {
+            int userId = 0;
+            var userIdClain = User.FindFirst("UsuariosId");
+            if (userIdClain != null && int.TryParse(userIdClain.Value, out int parsedUserId))
+                userId = parsedUserId;
+
+            Usuario usuario = _usuarioServicio.ObtenerUsuarioId(userId);
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarPerfil(Usuario model)
+        {
+            using (SqlConnection con = new SqlConnection(_contexto.Conexion))
+            {
+                using (SqlCommand cmd = new("ActualizarPerfil", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UsuariosId", model.UsuariosId);
+                    cmd.Parameters.AddWithValue("@Nombre", model.Nombre);
+                    cmd.Parameters.AddWithValue("@Apellido", model.Apellido);
+                    cmd.Parameters.AddWithValue("@Correo", model.Correo);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return RedirectToAction("Perfil");
+        }
+
+        [HttpPost]
+
+        public ActionResult EliminarCuenta()
+        {
+            int userId = 0;
+            var userIdClain = User.FindFirst("UsuariosId");
+            if (userIdClain != null && int.TryParse(userIdClain.Value, out int parsedUserId))
+                userId = parsedUserId;
+
+            using (SqlConnection con = new(_contexto.Conexion))
+            {
+                using (SqlCommand cmd = new("EliminarUsuario", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UsariosId", userId);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme); //Cierre de la sesion
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ InnoMasketss/Data/Email.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+public class Email
+{
+    // Se utiliza para enviar un correo electrónico al usuario
+    public void Enviar(string correo, string token)
+    {
+        Correo(correo, token);
+    }
+
+    // Configura el correo electrónico y lo envía usando el protocolo SMTP
+    void Correo(string correo_receptor, string token)
+    {
+        string correo_emisor = "paravideos404@gmail.com";
+        string clave_emisor = "xixz xzhp rdeu cqdg"; 
+
+        MailAddress receptor = new(correo_receptor);
+        MailAddress emisor = new(correo_emisor);
+
+        MailMessage email = new(emisor, receptor);
+        
+            email.Subject = "InnoMasketss: Activación de cuenta";
+            email.Body = @"<!DOCTYPE html>
+                           <html>
+                           <head>
+                               <title>Activación de cuenta</title>
+                           </head>
+                           <body>
+                               <h2>Activación de cuenta</h2>
+                               <p>Para activar su cuenta, haga clic en el siguiente enlace:</p>
+                               <a href='https://localhost:7131/Cuenta/Token?valor=" + token + "'>Activar cuenta</a></body> </html>";
+
+            email.IsBodyHtml = true;
+
+                SmtpClient smtp = new();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587; // Usamos el puerto correcto para TLS
+                smtp.EnableSsl = true; // Asegúrate de que esté habilitado el uso de SSL/TLS
+                smtp.Credentials = new NetworkCredential(correo_emisor, clave_emisor);
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Timeout = 10000; // Tiempo de espera opcional
+
+                try
+                {
+            smtp.Send(email);
+                }
+                catch (SmtpException)
+                {
+                    throw;
+                }
+            
+        
+    }
+}
